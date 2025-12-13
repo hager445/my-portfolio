d513f14d9bc1e8c20b8f40d5f3748b223b4df76c
hager445 <ho2333535@gmail.com>
2025-12-13 05:39:19 +0200

    editing cv

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,42 +1,45 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import Typed from 'typed.js';
-import { LucideAngularModule , Home, Info, ChevronsLeftRightEllipsis, Notebook, UserPen } from 'lucide-angular';
+import {
+  LucideAngularModule,
+  Home,
+  Info,
+  ChevronsLeftRightEllipsis,
+  Notebook,
+  UserPen,
+} from 'lucide-angular';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [  ],
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
- 
+  @ViewChild('typedContainer') typedContainer!: ElementRef;
 
+  ngAfterViewInit() {
+    const options = {
+      strings: [
+        'Crafting Stunning User Interfaces ✨',
+        'Building Interactive Web Experiences 🚀',
+        'Passionate About Angular ❤️',
+        'Turning Ideas Into Reality with Code 💡',
+      ],
+      typeSpeed: 50,
+      backSpeed: 25,
+      loop: true,
+    };
 
-@ViewChild('typedContainer') typedContainer!:ElementRef;
-
-ngAfterViewInit() {
-  const options = {
-    strings: [
-      "Crafting Stunning User Interfaces ✨",
-      "Building Interactive Web Experiences 🚀",
-      "Passionate About Angular ❤️",
-      "Turning Ideas Into Reality with Code 💡"
-    ]
-    ,
-    typeSpeed: 50,
-    backSpeed: 25,
-    loop: true
-  };
-
-  new Typed(this.typedContainer.nativeElement, options);
-}
-downloadFile():void{
- const link = document.createElement('a');
- link.href = "/assets/hager's resume.pdf"
- link.download = 'cv.pdf'
- document.body.appendChild(link);
- link.click();
- document.body.removeChild(link);
-}
+    new Typed(this.typedContainer.nativeElement, options);
+  }
+  downloadFile(): void {
+    const link = document.createElement('a');
+    link.href = '/assets/HAGER-MOSTAFA-Resume-20251209_251209_152849.pdf';
+    link.download = 'cv.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
